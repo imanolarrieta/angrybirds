c4baf0c0dede04bed9fcdafc39f74da209455269
imanolarrieta <imanol@stanford.edu>
2015-12-10 14:59:02 -0800

    Evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -52,6 +52,18 @@ def evaluator(rlAlgorithm,featureExtractor,nameAlg,nameFeat,numFeat = 64,numTria
             except AssertionError:
                 break
     elif nameAlg == 'RLSVI':
+        while trial<numTrials:
+            try:
+                print (trial)
+                rl.makeRSVI()
+                simulate(ab,rl,numTrials=1, maxIterations=1000, verbose=False, show=False)
+                rl.makeLSVI(0.0)
+                outcome = simulate(ab,rl,numTrials=1, maxIterations=1000, verbose=False, show=False)
+                levelsPassed = levelsPassed + outcome['levelsPassed']
+                totalRewards = totalRewards + outcome['totalRewards']
+                trial +=1
+            except AssertionError:
+                break
 
         while trial<numTrials:
             try:
@@ -112,7 +124,7 @@ def evaluator(rlAlgorithm,featureExtractor,nameAlg,nameFeat,numFeat = 64,numTria
             plt.axvline(i+1,color='r', linestyle='--')
     plt.savefig('../plots/totalRewards_'+name+'.png')
 
-def level_evaluator(level,rlAlgorithm,featureExtractor,nameAlg,nameFeat,numFeat = 64,numTrials=50, explorationProb = 0.3, sigma = 500)
+def level_evaluator(level,rlAlgorithm,featureExtractor,nameAlg,nameFeat,numFeat = 64,numTrials=50, explorationProb = 0.3, sigma = 500):
 
     ab = AngryBirdsMDP(level = level)
 
@@ -142,53 +154,23 @@ def level_evaluator(level,rlAlgorithm,featureExtractor,nameAlg,nameFeat,numFeat 
     plt.ylabel('Number of total rewards for level 3',fontsize='large')
     plt.title('Rewards per trials', fontsize=20)
     plt.savefig('totalRewardsL3.png')
+
+
 
 if __name__ == '__main__':
 
     agent = angryAgent()
-    evaluator(QLearningAlgorithm,agent.featureExtractorXYaction,'Q','PP',numFeat = 64,numTrials=50, explorationProb = 0.3, sigma = 500)
-    evaluator(QLearningAlgorithm,agent.nestedGridFeatureExtractor,'Q','NPP',numFeat = 64,numTrials=50, explorationProb = 0.3, sigma = 500)
-    evaluator(QLearningAlgorithm,agent.custom2FeatureExtractor,'Q','NPPO',numFeat = 64,numTrials=50, explorationProb = 0.3, sigma = 500)
-    evaluator(RLSVI,agent.featureExtractorXYaction,'RLSVI','PP',numFeat = 64,numTrials=50, explorationProb = 0.0, sigma = 500)
-    evaluator(RLSVI_wrapper,agent.nestedGridFeatureExtractor,'RLSVI','NPP',numFeat = 64,numTrials=50, explorationProb = 0.0, sigma = 500)
-    evaluator(RLSVI_wrapper,agent.custom2FeatureExtractor,'RLSVI','NPPO',numFeat = 64,numTrials=50, explorationProb = 0.0, sigma = 500)
-    evaluator(BRLSVI,agent.featureExtractorXYaction,'BRLSVI','PP',numFeat = 64,numTrials=50, explorationProb = 0.0, sigma = 500)
-    evaluator(BRLSVI,agent.nestedGridFeatureExtractor,'BRLSVI','NPP',numFeat = 64,numTrials=50, explorationProb = 0.0, sigma = 500)
-    evaluator(BRLSVI,agent.custom2FeatureExtractor,'BRLSVI','NPPO',numFeat = 64,numTrials=50, explorationProb = 0.0, sigma = 500)
-    evaluator(BRLSVI,agent.featureExtractorXYaction,'LSVI','PP',numFeat = 64,numTrials=50, explorationProb = 0.3, sigma = 500)
-    evaluator(BRLSVI,agent.nestedGridFeatureExtractor,'LSVI','NPP',numFeat = 64,numTrials=50, explorationProb = 0.3, sigma = 500)
-    evaluator(BRLSVI,agent.custom2FeatureExtractor,'LSVI','NPPO',numFeat = 64,numTrials=50, explorationProb = 0.3, sigma = 500)
+    evaluator(QLearningAlgorithm,agent.featureExtractorXYaction,'Q','PP',numFeat = 64,numTrials=50, epsilon = 0.3, sigma = 500)
+    evaluator(QLearningAlgorithm,agent.nestedGridFeatureExtractor,'Q','NPP',numFeat = 64,numTrials=50, epsilon = 0.3, sigma = 500)
+    evaluator(QLearningAlgorithm,agent.custom2FeatureExtractor,'Q','NPPO',numFeat = 64,numTrials=50, epsilon = 0.3, sigma = 500)
+    evaluator(RLSVI,agent.featureExtractorXYaction,'RLSVI','PP',numFeat = 64,numTrials=50, epsilon = 0.0, sigma = 500)
+    evaluator(RLSVI,agent.nestedGridFeatureExtractor,'RLSVI','NPP',numFeat = 64,numTrials=50, epsilon = 0.0, sigma = 500)
+    evaluator(RLSVI,agent.custom2FeatureExtractor,'RLSVI','NPPO',numFeat = 64,numTrials=50, epsilon = 0.0, sigma = 500)
+    evaluator(RLSVI,agent.featureExtractorXYaction,'LSVI','PP',numFeat = 64,numTrials=50, epsilon = 0.3, sigma = 500)
+    evaluator(RLSVI,agent.nestedGridFeatureExtractor,'LSVI','NPP',numFeat = 64,numTrials=50, epsilon = 0.3, sigma = 500)
+    evaluator(RLSVI,agent.custom2FeatureExtractor,'LSVI','NPPO',numFeat = 64,numTrials=50, epsilon = 0.3, sigma = 500)
 
-def level_evaluator(level,rlAlgorithm,featureExtractor,nameAlg,nameFeat,numFeat = 64,numTrials=50, explorationProb = 0.3, sigma = 500)
 
-    ab = AngryBirdsMDP(level = level)
-
-    rl = rlAlgorithm(actions=agent.getAngryBirdsActions,featureExtractor=featureExtractor,discount=ab.discount(),\
-                            explorationProb=explorationProb)
-    trainingOutcomes = simulate(ab,rl,numTrials=numTrials, maxIterations=1000, verbose=False, show=False)
-
-    explorationProb = 0.0
-    if nameAlg =='Q':
-        rl.setExplorationProb(explorationProb)
-    elif nameAlg=='LSVI':
-        rl.rlsvi.epsilon = explorationProb
-    else:
-        rl.rlsvi.sigma = 1.0
-    testOutcomes = simulate(ab,rl,numTrials=5,verbose = False, show=False)
-
-    print(trainingOutcomes)
-    print(testOutcomes)
-
-    totalRewards = trainingOutcomes['totalRewards']
-    trials = range(1,numTrials+1)
-
-    plt.figure()
-    plt.plot(trials,totalRewards,lw=2,color='green')
-    plt.scatter(trials,totalRewards)
-    plt.xlabel('Number of trials',fontsize='large')
-    plt.ylabel('Number of total rewards for level 3',fontsize='large')
-    plt.title('Rewards per trials', fontsize=20)
-    plt.savefig('totalRewardsL3.png')
 #
 # ############################################# Evaluation of Level 5
 #     ab = AngryBirdsMDP(level = 5)
